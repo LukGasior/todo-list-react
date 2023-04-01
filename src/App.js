8a901e473c1358289709975f291d1fed555144e7
Lukasz Gasior <lukasz.gasior7@gmail.com>
2023-04-01 11:05:53 +0200

    added core of todolist

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import Form from "./Form";
+import Tasks from "./Tasks";
+import Buttons from "./Buttons";
+import Header from "./Header";
+import Section from "./Section";
+import Container from "./Container";
+
+const tasks = [
+  {
+    id: 1,
+    content: "Wstań",
+    done: true,
+  },
+  {
+    id: 2,
+    content: "Umyj się",
+    done: false,
+  },
+];
+
+const taskHide = false;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Container>
+
+        <Header 
+        title={"Lista zadań"} 
+        />
+
+        <Section 
+        title={"Dodaj nowe zadanie"} 
+        body={<Form />} 
+        />
+
+        <Section
+        title={"Lista zadań"}
+        body={<Tasks tasks={tasks} taskHide={taskHide} />}
+        extraContent={<Buttons tasks={tasks} taskHide={taskHide} />}
+        />
+      </Container>
     </div>
   );
 }
