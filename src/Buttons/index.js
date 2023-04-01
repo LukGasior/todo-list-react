3ea46f48c0178c981c55cbd6f4550f0f94a2bc12
Lukasz Gasior <lukasz.gasior7@gmail.com>
2023-04-01 19:05:53 +0200

    non changes

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -2,20 +2,21 @@ import "./style.css";
 
 const Buttons = (props) => {
 
-    if(props.tasks.some(({done}) => done)) {
-       return ( 
-       <span className="form--buttons">
-    <>
-       <button onClick={props.toggleTaskHide} className="form__fieldset--button">
-           {props.taskHide ? "Pokaż ukończone" : "Ukryj ukończone"}
-       </button>
-       <button 
-       className="form__fieldset--button"
-       disabled={props.tasks.every(({done}) => done)}
-       >Ukryj Wszystkie</button>
-       
-   </>
-   </span> )
+    if (props.tasks.some(({ done }) => done)) {
+        return (
+            <span className="form--buttons">
+                <>
+                    <button onClick={props.toggleTaskHide}
+                        className="form__fieldset--button">
+                        {props.taskHide ? "Pokaż ukończone" : "Ukryj ukończone"}
+                    </button>
+                    <button
+                        className="form__fieldset--button"
+                        disabled={props.tasks.every(({ done }) => done)}
+                    >Ukryj Wszystkie</button>
+
+                </>
+            </span>)
     }
 }
 
